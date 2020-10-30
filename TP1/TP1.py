249237b326ab24cf292d18553a6a32744c98b060
mahishamimi <mahisha.uruthirasigamani@edu.ece.fr>
2020-10-30 20:49:57 +0100

    Update TP1.py

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -6,7 +6,15 @@ Created on Fri Oct 30 18:33:37 2020
 @author: rahimamohamed
 """
 
-print("hello")
+import numpy as np
+import panda as pd
 
-print("coucou")
-print("Mahisha")
+#Variables
+
+r=0.05 # Taux d'interet
+T=50 #  Temps
+
+X = [] # le capital investit
+W = [] # l'optimal cumulative reward
+A = [] # l'action au temps t
+Ro = [] # 

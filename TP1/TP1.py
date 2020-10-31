dd3bd482492945680d8ee4ed992d91a866da6896
Rahima-web <59868417+Rahima-web@users.noreply.github.com>
2020-10-31 14:39:03 +0100

    Update TP1.py

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -7,15 +7,27 @@ Created on Fri Oct 30 18:33:37 2020
 """
 
 import numpy as np
-import panda as pd
+import pandas as pd
 
-# Initialisation des variables
+" Variable declaration " 
 
-r=0.05 # Taux d'interet
-T=50 #  Temps
-
+r=0.05 # Interest rate
+T=50 #Maturity
 X = [] # le capital investit
 W = [] # l'optimal cumulative reward
 A = [] # l'action au temps t
 Ro = [] # Cumulative reward
 
+" Bang Bang Controller " 
+
+def BangBang_Controller():
+    Bang=1
+    for i in range (1,T):
+        if r>= 1/Bang:
+            Bang=(1+r) * Bang
+        if r< 1/Bang:
+            Bang = 1 + Bang
+    return Bang
+
+#x = BangBang_Controller()
+#print(x)

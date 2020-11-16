da634603ef6865a84c14b6a32669b14285ec3b1c
Rahima-web <59868417+Rahima-web@users.noreply.github.com>
2020-11-16 17:12:29 +0100

    Update TP3.py

--- TP3/TP3.py
+++ TP3/TP3.py
@@ -5,11 +5,13 @@ Created on Fri Nov 13 18:01:20 2020
 
 @author: rahimamohamed
 """
-
+import matplotlib.pyplot as plt
 from datetime import datetime 
 import numpy as np
 import pandas as pd
 from pyti.smoothed_moving_average import smoothed_moving_average as sma
+from pyti.bollinger_bands import lower_bollinger_band as lbb
+
 
 " VARIABLE DECLARATION" 
 
@@ -18,72 +20,130 @@ from pyti.smoothed_moving_average import smoothed_moving_average as sma
 
 data = pd.read_csv("https://raw.githubusercontent.com/Rahima-web/Hamiltonien/67773738288686dd932eb87932fd551b65b45f5d/TP3/bank_of_america.csv",sep=",",header=0)
 data['Date']=[datetime.strptime(x, '%m/%d/%Y') for x in data['Date'].str.slice(0,11)]
-data = data.drop(['Open', 'High','Low','Volume','Adj Close'], axis=1)
-data=data.to_numpy()
+#data = data.drop(['Open', 'High','Low','Volume','Adj Close'], axis=1)
+#data=data.to_numpy()
+
+
+
+data['fast_sma'] = sma(data['Close'].tolist(), 10)
+data['slow_sma'] = sma(data['Close'].tolist(), 30)
+
+
+
 
 
 " QUESTION 2: Separate the dataset in two parts: training and test datasets." 
 
-nb = int(len(data)*0.75)
-Train_date = data[0:nb,0]
-Train_close = data[0:nb,1]
+nb = int(len(data)*0.70)
+Train_date = data['Date'][:nb]
+Train_close = data['Close'][:nb]
 
-Test_date = data[nb:len(data),0]
-Test_close = data[nb:len(data),0]
+Test_date = data['Date'][nb:]
+Test_close = data['Close'][nb:]
+
+plt.plot(Train_date,Train_close,color="blue")
+plt.plot(Test_date,Test_close,color="red")
+plt.show()
 
 " QUESTION 3: Portfolio function" 
 
-n = len(Train_date)
+## Trading Strategy
 
-def policy(S): #POLICY DE MERDE TROUVER MIEUX
-    A = np.zeros((n,1))
-    for i in range(0,n):
-        if S[i] > np.mean(S):
-            A[i] = -1 #sell
-        if S[i] < np.mean(S):
-            A[i] = 1 #buy
-        if S[i] == np.mean(S):
-            A[i] = 0 #hold
-    return A
+def strategy(Data):	
+		
+		'''If Price is 3% below Slow Moving Average, then Buy
+		Put selling order for 2% above buying price'''
+        
+        A = np.zeros((n,1))
 
-A = policy(Train_close)
-NB_share = 30
-Cash = 5000
+		for i in range(1, len(df['close'])):
+			if data['slow_sma'][i] > data['low'][i] and (data['slow_sma'][i] - data['low'][i]) > 0.03 * data['low'][i]:
+				A[i]=1
 
-def reward(A,S,i):
-    R = np.zeros((n,1))
-    for j in range(0,n):
-        if A[j] == -1:
-            R[j]= -S[j] * NB_share
-        if A[i] == 1:
-            R[j]= S[j] * NB_share    
-        if A[j] == 0:
-            R[j]= 0
-    return R[i]
+            	buy_price = 0.8 * df['slow_sma'][i]
+		if buy_price >= df['close'][i]:
+			self.buy_signals.append([df['time'][i], df['close'][i], df['close'][i] * 1.045])
+			return True
 
 
-R = [reward(A,Train_close,i) for i in range(0,n)]
 
-def ptfs(R,i):
-    ptf = np.zeros((n,1))
-    ptf[0] = Cash
-    if i == 0: 
-        ptf[0] = Cash
-    else:
-        for j in range(0,n-1):
-            ptf[j+1] = ptf[j] + R[j]
-    return ptf[i]
 
-    
-ptf = [ptfs(R,i) for i in range(0,n)]
 
-alpha = 0.4
-gamma = 0.9
 
-def Q_learnigDEMERDE(S,A):
-    Q = np.zeros((n,1))
-    Q_prec = Q
-    for itera in range(10):        
-        for i in range(0,n):
-            
-            break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#n = len(Train_date)
+
+#def policy(S): 
+#    A = np.zeros((n,1))
+#    for i in range(0,n):
+#        if S[i] > np.mean(S):
+#            A[i] = -1 #sell
+#        if S[i] < np.mean(S):
+#            A[i] = 1 #buy
+#        if S[i] == np.mean(S):
+#            A[i] = 0 #hold
+#    return A
+#
+#
+#A = policy(Train_close)
+#NB_share = 30
+#Cash = 5000
+
+#def reward(A,S,i):
+#    R = np.zeros((n,1))
+#    for j in range(0,n):
+#        if A[j] == -1:
+#            R[j]= -S[j] * NB_share
+#        if A[i] == 1:
+#            R[j]= S[j] * NB_share    
+#        if A[j] == 0:
+#            R[j]= 0
+#    return R[i]
+#
+#
+#R = [reward(A,Train_close,i) for i in range(0,n)]
+#
+#def ptfs(R,i):
+#    ptf = np.zeros((n,1))
+#    ptf[0] = Cash
+#    if i == 0: 
+#        ptf[0] = Cash
+#    else:
+#        for j in range(0,n-1):
+#            ptf[j+1] = ptf[j] + R[j]
+#    return ptf[i]
+#
+#    
+#ptf = [ptfs(R,i) for i in range(0,n)]
+#
+#alpha = 0.4
+#gamma = 0.9
+#
+#def Q_learnig(S,A):
+#    Q = np.zeros((n,1))
+#    Q_prec = Q
+#    for itera in range(10):        
+#        for i in range(0,n):
+#            
+#            break

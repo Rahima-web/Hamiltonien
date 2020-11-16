e53b688870e4125318363140fc6a1a489ee221ed
Rahima-web <59868417+Rahima-web@users.noreply.github.com>
2020-11-16 16:10:11 +0100

    Update TP3.py

--- TP3/TP3.py
+++ TP3/TP3.py
@@ -9,6 +9,7 @@ Created on Fri Nov 13 18:01:20 2020
 from datetime import datetime 
 import numpy as np
 import pandas as pd
+from pyti.smoothed_moving_average import smoothed_moving_average as sma
 
 " VARIABLE DECLARATION" 
 
@@ -23,10 +24,66 @@ data=data.to_numpy()
 
 " QUESTION 2: Separate the dataset in two parts: training and test datasets." 
 
-nb = int(len(data()*0.7))
+nb = int(len(data)*0.75)
 Train_date = data[0:nb,0]
 Train_close = data[0:nb,1]
 
 Test_date = data[nb:len(data),0]
 Test_close = data[nb:len(data),0]
+
 " QUESTION 3: Portfolio function" 
+
+n = len(Train_date)
+
+def policy(S): #POLICY DE MERDE TROUVER MIEUX
+    A = np.zeros((n,1))
+    for i in range(0,n):
+        if S[i] > np.mean(S):
+            A[i] = -1 #sell
+        if S[i] < np.mean(S):
+            A[i] = 1 #buy
+        if S[i] == np.mean(S):
+            A[i] = 0 #hold
+    return A
+
+A = policy(Train_close)
+NB_share = 30
+Cash = 5000
+
+def reward(A,S,i):
+    R = np.zeros((n,1))
+    for j in range(0,n):
+        if A[j] == -1:
+            R[j]= -S[j] * NB_share
+        if A[i] == 1:
+            R[j]= S[j] * NB_share    
+        if A[j] == 0:
+            R[j]= 0
+    return R[i]
+
+
+R = [reward(A,Train_close,i) for i in range(0,n)]
+
+def ptfs(R,i):
+    ptf = np.zeros((n,1))
+    ptf[0] = Cash
+    if i == 0: 
+        ptf[0] = Cash
+    else:
+        for j in range(0,n-1):
+            ptf[j+1] = ptf[j] + R[j]
+    return ptf[i]
+
+    
+ptf = [ptfs(R,i) for i in range(0,n)]
+
+alpha = 0.4
+gamma = 0.9
+
+def Q_learnigDEMERDE(S,A):
+    Q = np.zeros((n,1))
+    Q_prec = Q
+    for itera in range(10):        
+        for i in range(0,n):
+            
+            break

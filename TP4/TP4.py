736589d5dfdbca16e237cd997201f9efd4cee86e
Rahima-web <59868417+Rahima-web@users.noreply.github.com>
2020-11-22 14:27:44 +0100

    Create TP4.py

---
+++ TP4/TP4.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 22 14:21:23 2020
+
+@author: rahimamohamed
+
+GROUPE : Rahima Mohamed, Mahisha Uruthirasigamani, Danush Chandrarajah  
+
+PROBLEM : You look for a parking space on the street. Each space is free with probability p = 1 − q. 
+You cannot tell if a space is free until you reach it. You can not go backward. Once at a space, 
+you must decide to stop or to continue. 
+From position s (i.e., s spaces from your destination), the cost of stopping is s. 
+The cost of passing your destination without parking is D. Construct the strategy
+that will return the optimal parking space for a destination.
+
+Note: Fix yourself (at your will) the values that you need to solve the problem
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+""" QUESTION 1 : Generate several maps with different parking-space distributions. """
+
+
+
+
+
+
+""" QUESTION 2 : Implement the parking strategy shown in class for the generated maps."""
+
+
+
+
+""" QUESTION 3 : Discuss your results. """
